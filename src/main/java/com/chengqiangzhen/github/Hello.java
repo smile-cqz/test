f6b8ee39bd6a4077f41184ded9d6c4a8a12e32a5
smilecqz <1015706365@qq.com>
2020-04-24 16:29:06 +0800

    my new file Hello.java

--- src/main/java/com/chengqiangzhen/github/Hello.java
+++ src/main/java/com/chengqiangzhen/github/Hello.java
@@ -2,6 +2,6 @@ package com.chengqiangzhen.github;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("hello world cqz");
+        System.out.println("hello world cqz12");
     }
 }
